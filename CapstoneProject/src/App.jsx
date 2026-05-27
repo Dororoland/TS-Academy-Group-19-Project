930a601422a26990d1fbd406abd0fb55b33110f4
Dororoland <Dororolandoghenerume@gmail.com>
2026-05-27 16:34:39 +0100

    Feat/Image-Structure

--- CapstoneProject/src/App.jsx
+++ CapstoneProject/src/App.jsx
@@ -3,11 +3,32 @@ import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
 import './App.css'
+import Imagesection from './ImageSection/imagesection.jsx'
+
+
 
 function App() {
   const [count, setCount] = useState(0)
 
+  return (
+    <div>
+      
+     <Imagesection/>
+
+
+
+  </div>
   
+
+
+
+
+
+)
+
+
+  
+
 }
 
 export default App
---
+++ CapstoneProject/src/ImageSection/imagesection.jsx
@@ -0,0 +1,27 @@
+import './imagesection.css'
+
+
+
+function Imagesection() {
+return (
+<div>
+<div><h2 className="topic">Visualizing the Differences Between Planets</h2>
+    <p className="text">Each planet in our solar system has unique physical characteristics. Visual comparisons help highlight<br />
+        how vastly different terrestrial planets are from gas giants and ice giants.</p></div>
+        <div className ="gridcontainer">
+            <div className="box">1 </div>
+            <div className="box">2 </div>
+            <div className="box">3 </div>
+            <div className="box">4 </div>
+            <div className="box">5 </div>
+            <div className="box">6 </div>
+            <div className="box">7 </div>
+            <div className="box">8 </div>
+            <div className="box">9 </div>
+        </div>
+</div>
+
+)
+}
+
+export default Imagesection
