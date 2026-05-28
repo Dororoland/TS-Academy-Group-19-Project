b6cc98d25e3d16c7c23ad49f72a78879b04f8437
Dororoland <Dororolandoghenerume@gmail.com>
2026-05-28 09:45:54 +0100

    Image Section

--- CapstoneProject/src/ImageSection/imagesection.jsx
+++ CapstoneProject/src/ImageSection/imagesection.jsx
@@ -1,27 +1,50 @@
 import './imagesection.css'
-
-
+import { useEffect, useState } from "react";
 
 function Imagesection() {
-return (
-<div>
-<div><h2 className="topic">Visualizing the Differences Between Planets</h2>
-    <p className="text">Each planet in our solar system has unique physical characteristics. Visual comparisons help highlight<br />
-        how vastly different terrestrial planets are from gas giants and ice giants.</p></div>
-        <div className ="gridcontainer">
-            <div className="box">1 </div>
-            <div className="box">2 </div>
-            <div className="box">3 </div>
-            <div className="box">4 </div>
-            <div className="box">5 </div>
-            <div className="box">6 </div>
-            <div className="box">7 </div>
-            <div className="box">8 </div>
-            <div className="box">9 </div>
-        </div>
-</div>
+  const [planets, setPlanets] = useState([]);
 
+  useEffect(() => {
+    fetch("https://anurella.github.io/json/planets.json")
+      .then((res) => res.json())
+      .then((data) => setPlanets(data))
+      .catch((err) => console.log(err));
+  }, [])
+
+return (
+    <section>
+<h2 className="topic">Visualizing the Differences Between Planets</h2>
+    <p className="text">Each planet in our solar system has unique physical characteristics. Visual comparisons help highlight
+        how vastly different terrestrial planets are from gas giants and ice giants.</p>
+       <div className="container">
+    
+
+      <div className="grid">
+        {planets.map((planet, index) => (
+          
+          <div className="card" key={index}>
+            <figure>
+            <img src={planet.image} alt={planet.planet}/>
+            <figcaption>{planet.planet}<br/>
+            {planet.distanceFromSun} million km</figcaption>
+            </figure>
+          </div>
+        ))}
+      </div>
+    </div>
+
+   </section>
 )
 }
 
+
+
+
 export default Imagesection
+
+
+
+
+ 
+ 
+    
