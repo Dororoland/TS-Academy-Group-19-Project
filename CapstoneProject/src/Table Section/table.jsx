9427b5a9bc2ee0288e63e91c1ee5745711b93a20
oshoalaraodat <oshoalaraodat@gmail.com>
2026-05-30 14:14:38 +0100

    feat: i worked on the table.jsx

--- CapstoneProject/src/Table Section/table.jsx
+++ CapstoneProject/src/Table Section/table.jsx
@@ -1,6 +1,95 @@
 export default function table(){
     return(
-        
+  <div>
+        <h3><b>Planetry Fact at a Glance</b></h3>
+   <p> Below is a comparative table of major planets in our solar system. The data highlight key physical<br> properties used by astronomers and researchers worldwide.</br></p>
+   <p><strong>Data about the planets of our solar system (Planetary facts taken from NASA)</strong></p>
+   <table border="1" cellpadding="10" cellspacing="0" width="100%">
+  <tr>
+    <th></th>
+    <th></th>
+    <th>Name</th>
+    <th>Mass (10 24kg)</th>
+    <th>Diameter (km)</th>
+    <th>Density (kg/m3)</th>
+    <th>Gravity (m/s2)</th>
+  </tr>
+
+  <tr>
+    <td rowspan="4">Terrestrial Planets</td>
+    <td rowspan="4"></td>
+    <td>Mercury</td>
+    <td>0.330</td>
+    <td>4,878</td>
+    <td>5427</td>
+    <td>3.7</td>
+  </tr>
+  <tr>
+    <td>Venus</td>
+    <td>0.330</td>
+    <td>4,878</td>
+    <td>5427</td>
+    <td>3.7</td>
+  </tr>
+  <tr>
+    <td>Earth</td>
+    <td>0.330</td>
+    <td>4,878</td>
+    <td>5427</td>
+    <td>3.7</td>
+  </tr>
+  <tr>
+    <td>Mars</td>
+    <td>0.330</td>
+    <td>4,878</td>
+    <td>5427</td>
+    <td>3.7</td>
+  </tr>
+
+  
+  <tr>
+    <td rowspan="4">Jovian Planets</td>
+    <td rowspan="2">Gas Giants</td>
+    <td>Jupiter</td>
+    <td>0.330</td>
+    <td>4,878</td>
+    <td>5427</td>
+    <td>3.7</td>
+  </tr>
+  <tr>
+    <td>Saturn</td>
+    <td>0.330</td>
+    <td>4,878</td>
+    <td>5427</td>
+    <td>3.7</td>
+  </tr>
+  <tr>
+    <td rowspan="2">Ice Giants</td>
+    <td>Uranus</td>
+    <td>0.330</td>
+    <td>4,878</td>
+    <td>5427</td>
+    <td>3.7</td>
+  </tr>
+  <tr>
+    <td>Neptune</td>
+    <td>0.330</td>
+    <td>4,878</td>
+    <td>5427</td>
+    <td>3.7</td>
+  </tr>
+
+  <tr>
+    <td rowspan="2">Dwarf Planets</td>
+    <td></td>
+    <td>Pluto</td>
+    <td>0.330</td>
+    <td>4,878</td>
+    <td>5427</td>
+    <td>3.7</td>
+  </tr>
+</table>
+    </div>
     )
 
 }
