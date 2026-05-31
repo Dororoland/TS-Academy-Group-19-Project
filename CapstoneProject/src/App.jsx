236f8d83aaf32649c98468eb6755b60e7edbdbc4
Odunayo <odunayoemmyb@gmail.com>
2026-05-31 09:19:14 -0700

    add form-structure css

--- CapstoneProject/src/App.jsx
+++ CapstoneProject/src/App.jsx
@@ -1,13 +1,20 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
+import Form from './Form Section/form'
 import './App.css'
+import FormSec from './Form Section/form'
 
 function App() {
   const [count, setCount] = useState(0)
-
+  return (
   
+    <div>
+      <FormSec/>
+    </div>
+  )
 }
+  
+
 
 export default App
